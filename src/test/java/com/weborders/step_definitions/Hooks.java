3fa2dde45b4b5c0f8b2042e3023cf48683cfa81d
vvyugovskiy <resifar@gmail.com>
2020-04-22 23:55:30 -0400

    utils

---
+++ src/test/java/com/weborders/step_definitions/Hooks.java
@@ -0,0 +1,27 @@
+package com.weborders.step_definitions;
+
+import com.weborders.utilities.Driver;
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @Before
+    public void setup(Scenario scenario) {
+        System.out.println("Test scenario :: " + scenario.getName());
+        Driver.getDriver().manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+            byte[] image = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+            scenario.embed(image, "image/png", scenario.getName());
+        }
+        Driver.closeDriver();
+    }
+}
